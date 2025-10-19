1a80f5a0eae44c257d3fa5b6e72502f8f9befc68
Ren Toda <renpetertoda@gmail.com>
2025-10-19 09:48:58 +0000

    django not woking

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,14 @@ from django.urls import path, include
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+
+    path('glitchtip-debug/', trigger_error),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
