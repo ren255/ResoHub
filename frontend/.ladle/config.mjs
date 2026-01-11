957a72065fd25704b44854b1ae8e0947f869a5ae
Ren Toda <renpetertoda@gmail.com>
2026-01-11 15:04:29 +0900

    added ladle. component library

---
+++ frontend/.ladle/config.mjs
@@ -0,0 +1,6 @@
+// .ladle/config.mjs
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+  stories: "components/**/*.stories.{js,jsx,ts,tsx,mdx}",
+  viteConfig: "./ladle-vite.config.ts",
+};
---
+++ frontend/components/hello.stories.tsx
@@ -0,0 +1,2 @@
+export const World = () => <p>Hey???</p>;
+export const Third = () => <p>sann</p>;
