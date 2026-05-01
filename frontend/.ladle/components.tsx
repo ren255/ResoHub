9016d3b57e7ef8a5f35bda54f3c18b8c4b68f6e3
ren255 <renpetertoda@gmail.com>
2026-05-01 10:42:28 +0900

    fix ladle loading

--- frontend/.ladle/components.tsx
+++ frontend/.ladle/components.tsx
@@ -3,8 +3,10 @@ import "src/layouts/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 import type { Story } from "@ladle/react";
 
-export const Provider = ({ children }: { children: React.ReactNode }) => (
-    <BrowserRouter>
-        {children}
-    </BrowserRouter>
-);
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <BrowserRouter>
+            {children}
+        </BrowserRouter>
+    );
+};
--- frontend/ladle-vite.config.ts
+++ frontend/ladle-vite.config.ts
@@ -1,9 +1,19 @@
 // ladle-vite.config.ts
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@features": resolve(__dirname, "./src/components/features"),
+      "@layouts": resolve(__dirname, "./src/components/layouts"),
+      "@stories": resolve(__dirname, "./src/components/stories"),
+      "@ui": resolve(__dirname, "./src/components/ui"),
+    },
+  },
   build: {
     target: "es2022",
   },
